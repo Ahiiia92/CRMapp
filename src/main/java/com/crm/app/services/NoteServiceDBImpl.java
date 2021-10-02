6bab8eae3281b342fe69d159ef2022faafff2c8b
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-10-02 22:31:06 +0200

    fixed NoteServiceDBImpl class

--- src/main/java/com/crm/app/services/NoteServiceDBImpl.java
+++ src/main/java/com/crm/app/services/NoteServiceDBImpl.java
@@ -1,7 +1,19 @@
 package com.crm.app.services;
 
+import com.crm.app.models.Note;
+import com.crm.app.repositories.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class NoteServiceDBImpl implements NoteService {
+    @Autowired
+    private NoteRepository repo;
+
+    @Override
+    public List<Note> getAllNotes() {
+        return repo.findAll();
+    }
 }
